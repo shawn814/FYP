6b09efc060530e8375b29b168db39fa9facac437
lim jiaying <kerrykerry123456789@hotmail.com>
2020-05-17 15:15:34 +0800

    done exercise

---
+++ example7.vue
@@ -0,0 +1,20 @@
+<template>
+     <div>
+            <p> {{item.id}}</p>
+            <p @click="toggle =! toggle">Details</p>
+            <p v-if="toggle">{{ item.description}}</p>
+            <p>{{ii}}</p>
+            =======================================
+        </div>
+</template>
+<script>
+export default {
+    name:"pp",
+    props:['item'],
+    data:function(){
+        return{
+            toggle:false,
+        }
+    }
+}
+</script>
