f156efde9417057707f9fe7d34a95b040c11268b
lim jiaying <kerrykerry123456789@hotmail.com>
2020-05-18 23:12:36 +0800

    Co-authored-by: Nchen118 <nchen118@yahoo.com> Co-authored-by: thecommonnamejane <thecommonnamejane@gmail.com>

---
+++ router.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+if (isset($_REQUEST['code'])) {
+    $fp = fopen('data.dat', "w");
+    fputs($fp, $_REQUEST['code']);
+    fclose($fp);
+}
+else {
+    while (true) {
+        $code = file_get_contents("data.dat");
+
+        if (strcmp($code, $_SESSION['code']) != 0) {
+            echo "data: $code\n\n";
+        }
+
+        $_SESSION['code'] = $code;
+
+        ob_flush();
+        flush();
+        sleep(1);
+    }
+}
+
+?>
